d60fae59ddfcf7522c627fe377b9770ec7b77fc6
Frédéric MARTIN <frederic.martin.fma@gmail.com>
2021-09-10 21:27:18 +0200

    Adding more samples with CellBuilder and ComplexCellBuilder

--- vhatable/sample.py
+++ vhatable/sample.py
@@ -33,13 +33,56 @@ import argparse
 import logging
 
 from linshareapi.core import ResourceBuilder
+from vhatable.core import Action
 from vhatable.core import TableFactory
+from vhatable.cell import CellBuilder
+from vhatable.cell import ComplexCellBuilder
 from vhatable.filters import PartialOr
 from vhatable.filters import PartialDate
 
 
+class AbstractEndpoint:
+    """Abstract class for all endpoint classes"""
 
-class Endpoint1:
+    last_id = 0
+    my_list = []
+
+    def _get_next_id(self):
+        self.last_id += 1
+        return self.last_id
+
+    def list(self):
+        """Return all elements from the list"""
+        return self.my_list
+
+    def get(self, elt_id):
+        """Get a element from the list"""
+        for elt in self.my_list:
+            if elt["id"] == elt_id:
+                return elt
+        return None
+
+    def delete(self, elt_id):
+        """Delete an element from the list"""
+        found = False
+        for elt in self.my_list:
+            if elt["id"] == elt_id:
+                self.my_list.remove(elt)
+                found = True
+        return found
+
+    def update(self, obj):
+        """Update an element from the list, only name is supported."""
+        found = False
+        for elt in self.my_list:
+            if elt["id"] == obj["id"]:
+                elt["name"] = obj["name"]
+                elt["modificationDate"] = time.time()
+                found = True
+        return found
+
+
+class Endpoint1(AbstractEndpoint):
     """Sample of an Endpoint1"""
 
     def __init__(self):
@@ -77,21 +120,6 @@ class Endpoint1:
         ]
         self.last_id = 5
 
-    def _get_next_id(self):
-        self.last_id += 1
-        return self.last_id
-
-    def list(self):
-        """Return all elements from the list"""
-        return self.my_list
-
-    def get(self, elt_id):
-        """Get a element from the list"""
-        for elt in self.my_list:
-            if elt["id"] == elt_id:
-                return elt
-        return None
-
     def create(self, obj):
         """Create an element and add it to the list"""
         elt = {}
@@ -102,30 +130,165 @@ class Endpoint1:
         self.my_list.append(elt)
         return elt
 
-    def delete(self, elt_id):
-        """Delete an element from the list"""
-        found = False
-        for elt in self.my_list:
-            if elt["id"] == elt_id:
-                self.my_list.remove(elt)
-                found = True
-        return found
-
-    def update(self, obj):
-        """Update an element from the list, only name is supported."""
-        found = False
-        for elt in self.my_list:
-            if elt["id"] == obj["id"]:
-                elt["name"] = obj["name"]
-                elt["modificationDate"] = time.time()
-                found = True
-        return found
-
     def get_rbu(self):
         """FIXME"""
+        # pylint: disable=no-self-use
         rbu = ResourceBuilder("element")
         rbu.add_field('id')
         rbu.add_field('name', required=True)
+        rbu.add_field('creationDate')
+        rbu.add_field('modificationDate', extended=True)
+        return rbu
+
+
+class Endpoint2(AbstractEndpoint):
+    """Sample of an Endpoint2"""
+
+    def __init__(self):
+        self.my_list = [
+            {
+                "creationDate": 1625647954855,
+                "modificationDate": 1625647954855,
+                "name": "My first element",
+                "foo": 0.01,
+                "id": 1
+            },
+            {
+                "creationDate": 1630922388966,
+                "modificationDate": 1630922389147,
+                "name": "My second element",
+                "foo": 0.07,
+                "id": 2
+            },
+            {
+                "creationDate": 1631023612633,
+                "modificationDate": 1631023612862,
+                "name": "My third element",
+                "foo": 0.1,
+                "id": 3
+            },
+            {
+                "creationDate": 1631105647241,
+                "modificationDate": 1631105647309,
+                "name": "My forth element",
+                "foo": 0.2,
+                "id": 4
+            },
+            {
+                "creationDate": 1631194331337,
+                "modificationDate": 1631194331444,
+                "name": "My fifth element",
+                "foo": 0.43,
+                "id": 5
+            }
+        ]
+        self.last_id = 5
+
+    def create(self, obj):
+        """Create an element and add it to the list"""
+        elt = {}
+        elt["id"] = self._get_next_id()
+        elt["name"] = obj["name"]
+        elt["creationDate"] = time.time()
+        elt["modificationDate"] = time.time()
+        elt["foo"] = 0.02
+        self.my_list.append(elt)
+        return elt
+
+    def get_rbu(self):
+        """FIXME"""
+        # pylint: disable=no-self-use
+        rbu = ResourceBuilder("element")
+        rbu.add_field('id')
+        rbu.add_field('name', required=True)
+        rbu.add_field('foo')
+        rbu.add_field('creationDate')
+        rbu.add_field('modificationDate', extended=True)
+        return rbu
+
+
+class Endpoint3(AbstractEndpoint):
+    """Sample of an Endpoint2"""
+
+    def __init__(self):
+        self.my_list = [
+            {
+                "creationDate": 1625647954855,
+                "modificationDate": 1625647954855,
+                "name": "My first element",
+                "foo": 0.01,
+                "foo_complex": {
+                    "name": "nested object",
+                    "uuid": "f05e7292-e00e-4bd8-94e2-3f9c3316071f"
+                },
+                "id": 1
+            },
+            {
+                "creationDate": 1630922388966,
+                "modificationDate": 1630922389147,
+                "name": "My second element",
+                "foo": 0.07,
+                "foo_complex": {
+                    "name": "nested object",
+                    "uuid": "f06e7292-e00e-4bd8-94e2-3f9c3316071f"
+                },
+                "id": 2
+            },
+            {
+                "creationDate": 1631023612633,
+                "modificationDate": 1631023612862,
+                "name": "My third element",
+                "foo": 0.1,
+                "foo_complex": {
+                    "name": "nested object",
+                    "uuid": "f07e7292-e00e-4bd8-94e2-3f9c3316071f"
+                },
+                "id": 3
+            },
+            {
+                "creationDate": 1631105647241,
+                "modificationDate": 1631105647309,
+                "name": "My forth element",
+                "foo": 0.2,
+                "foo_complex": {
+                    "name": "nested object",
+                    "uuid": "f08e7292-e00e-4bd8-94e2-3f9c3316071f"
+                },
+                "id": 4
+            },
+            {
+                "creationDate": 1631194331337,
+                "modificationDate": 1631194331444,
+                "name": "My fifth element",
+                "foo": 0.43,
+                "foo_complex": {
+                    "name": "nested object",
+                    "uuid": "f09e7292-e00e-4bd8-94e2-3f9c3316071f"
+                },
+                "id": 5
+            }
+        ]
+        self.last_id = 5
+
+    def create(self, obj):
+        """Create an element and add it to the list"""
+        elt = {}
+        elt["id"] = self._get_next_id()
+        elt["name"] = obj["name"]
+        elt["creationDate"] = time.time()
+        elt["modificationDate"] = time.time()
+        elt["foo"] = 0.02
+        self.my_list.append(elt)
+        return elt
+
+    def get_rbu(self):
+        """FIXME"""
+        # pylint: disable=no-self-use
+        rbu = ResourceBuilder("element")
+        rbu.add_field('id')
+        rbu.add_field('name', required=True)
+        rbu.add_field('foo')
+        rbu.add_field('foo_complex')
         rbu.add_field('creationDate')
         rbu.add_field('modificationDate', extended=True)
         return rbu
@@ -137,12 +300,33 @@ class API:
 
     def __init__(self):
         self.endpoint1 = Endpoint1()
+        self.endpoint2 = Endpoint2()
+        self.endpoint3 = Endpoint3()
 
 
+class SomeTitle(Action):
+    """TODO"""
 
-def helper_print_header():
+    display = True
+
+    def init(self, args, cli, endpoint):
+        super(SomeTitle, self).init(args, cli, endpoint)
+        self.display = not getattr(args, 'no_title', False)
+
+    def __call__(self, args, cli, endpoint, data):
+        self.init(args, cli, endpoint)
+        if self.display:
+            print()
+            print("###> Some Title")
+            print()
+
+
+def helper_print_header(extra=None):
+    """Just a tiny helper to display a header between each rendering."""
     print("")
     print("=========================================")
+    if extra:
+        print(extra)
     print("")
 
 def get_default_args():
@@ -152,7 +336,9 @@ def get_default_args():
     args = argparse.Namespace()
     args.verbose = False
     args.extended = False
-    # args.debug = False
+    args.vertical = False
+    args.raw = False
+    args.debug = 0
     return args
 
 def sample1():
@@ -238,20 +424,90 @@ def sample5():
     tbu.load_args(args).build().load_v2(api.endpoint1.list()).render()
 
 def sample6():
+    """Using some title/header to the list."""
+    args = get_default_args()
+    api = API()
+    tbu = TableFactory(api, api.endpoint1, "creationDate")
+
+    helper_print_header("    >>>> Without a title <<<<")
+    args.no_title=True
+    tbu.load_args(args).build().load_v2(api.endpoint1.list()).render()
+
+    helper_print_header("    >>>> With a title <<<<")
+    args.no_title=False
+    tbu.add_pre_render_class(SomeTitle())
+    tbu.load_args(args).build().load_v2(api.endpoint1.list()).render()
+
+def sample7():
+    """Using CellBuilder to format the content of a colum."""
+    args = get_default_args()
+    # args.debug = 3
+    api = API()
+    tbu = TableFactory(api, api.endpoint2, "creationDate")
+    # https://docs.python.org/3/library/string.html#format-specification-mini-language
+    tbu.add_custom_cell("foo", CellBuilder('{value:.2%}', '{value:.8%}'))
+
+    helper_print_header("    >>>> Raw data <<<<")
+    args.raw = True
+    tbu.load_args(args).build().load_v2(api.endpoint2.list()).render()
+
+    helper_print_header("    >>>> Formated data <<<<")
+    args.raw = False
+    tbu.load_args(args).build().load_v2(api.endpoint2.list()).render()
+
+def sample8():
+    """Using CellBuilder and vertical mode """
+    args = get_default_args()
+    api = API()
+    tbu = TableFactory(api, api.endpoint2, "creationDate")
+    # https://docs.python.org/3/library/string.html#format-specification-mini-language
+    tbu.add_custom_cell("foo", CellBuilder('{value:.2%}', '{value:.8%}'))
+    # CellBuilder('horizontal format', 'vertical format', 'filter format'))
+    # When you will try to use a Equal filter for example, 'filter format' will
+    # be used to format the content of the cell before testing the equality.
+
+    helper_print_header("    >>>> Horizontal mode <<<<")
+    tbu.load_args(args).build().load_v2(api.endpoint2.list()).render()
+
+    helper_print_header("    >>>> Vertical mode <<<<")
+    args.vertical = True
+    tbu.load_args(args).build().load_v2(api.endpoint2.list()).render()
+
+def sample9():
+    """Using ComplexCellBuilder and vertical mode """
+    args = get_default_args()
+    api = API()
+    tbu = TableFactory(api, api.endpoint3, "creationDate")
+    # https://docs.python.org/3/library/string.html#format-specification-mini-language
+    tbu.add_custom_cell("foo", CellBuilder('{value:.2%}', '{value:.8%}'))
+    # CellBuilder('horizontal format', 'vertical format', 'filter format'))
+    # When you will try to use a Equal filter for example, 'filter format' will
+    # be used to format the content of the cell before testing the equality.
+    tbu.add_custom_cell("foo_complex", ComplexCellBuilder(
+        '{name} ({uuid:.8})',
+        '{name} ({uuid})'
+    ))
+
+    helper_print_header("    >>>> Horizontal mode <<<<")
+    tbu.load_args(args).build().load_v2(api.endpoint3.list()).render()
+
+    helper_print_header("    >>>> Vertical mode <<<<")
+    args.vertical = True
+    tbu.load_args(args).build().load_v2(api.endpoint3.list()).render()
+
+def sample10():
     """TODO"""
     args = get_default_args()
     api = API()
     tbu = TableFactory(api, api.endpoint1, "creationDate")
     # tbu.add_action
-    # tbu.add_custom_cell
-    # tbu.add_pre_render_class
     # tbu.add_filter_cond
     helper_print_header()
     tbu.load_args(args).build().load_v2(api.endpoint1.list()).render()
 
 def main():
     """ Main entrypoint of this sample program."""
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     map_sample = {}
     for name,obj in inspect.getmembers(sys.modules[__name__]):
         if inspect.isfunction(obj) and name.startswith('sample'):
